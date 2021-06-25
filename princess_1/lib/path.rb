aaddc3cf7e68da5c3b6bdbd279d440c5b2ae83ee
Jordan Beck <jordanfbeck@gmail.com>
2021-06-25 12:59:32 -0600

    Refactor path make_path method in princess_1

--- princess_1/lib/path.rb
+++ princess_1/lib/path.rb
@@ -1,6 +1,14 @@
 class Path
   def self.make_path(m, bot_location, princess_location)
-    moves = []
+    negative_indice_cleaner(princess_location, bot_location)
+
+    first_move = (bot_location[0] - princess_location[0]).abs
+    second_move = (bot_location[1] - princess_location[1]).abs
+
+    directions(first_move, second_move, bot_location, princess_location)
+  end
+
+  def self.negative_indice_cleaner(bot_location, princess_location)
     if princess_location[0] < 0
       princess_location[0] = size - 1
     end
@@ -13,10 +21,10 @@ class Path
     if bot_location[1] < 0
       bot_location[1] = size - 1
     end
+  end
 
-    first_move = (bot_location[0] - princess_location[0]).abs
-    second_move = (bot_location[1] - princess_location[1]).abs
-
+  def self.directions(first_move, second_move, bot_location, princess_location)
+    moves = []
     if bot_location[0] < princess_location[0]
       moves << 'DOWN,' * first_move
     else
