6295efc8175fffaa1cffb8090e5a1c9b1aa32b78
Jordan Beck <jordanfbeck@gmail.com>
2021-06-25 19:12:25 -0600

    Adds refactor for princess class method into module

---
+++ modules/princess_helper_module.rb
@@ -0,0 +1,13 @@
+module PrincessHelper
+  def locate_princess(grid)
+    grid.each_with_index do |element, index|
+      if element.include?("p")
+        element.chars.each_with_index do |e, i|
+          if e == "p"
+            return [index, i]
+          end
+        end
+      end
+    end
+  end
+end
--- princess_1/lib/princess.rb
+++ princess_1/lib/princess.rb
@@ -1,13 +1,9 @@
+require '../modules/princess_helper_module'
+
 class Princess
-  def self.locate_princess(grid)
-    grid.each_with_index do |element, index|
-      if element.include?("p")
-        element.chars.each_with_index do |e, i|
-          if e == "p"
-            return [index, i]
-          end
-        end
-      end
-    end
+  extend PrincessHelper
+
+  def locate_the_princess(grid)
+    locate_princess(grid)
   end
 end
