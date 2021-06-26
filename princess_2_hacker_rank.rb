8d80b83b43ee098cb81f3af8af87d615da634093
Jordan Beck <jordanfbeck@gmail.com>
2021-06-26 10:12:56 -0600

    Adds refactor to hacker rank file, dynamic character location

--- princess_2_hacker_rank.rb
+++ princess_2_hacker_rank.rb
@@ -18,23 +18,11 @@ def make_path(m, bot_location, princess_location)
   moves.join.gsub(",", "\n")
 end
 
-def locate_bot(grid)
+def locate_character(grid, character)
   grid.each_with_index do |element, index|
-    if element.include?("m")
+    if element.include?(character)
       element.chars.each_with_index do |e, i|
-        if e == "m"
-          return [index, i]
-        end
-      end
-    end
-  end
-end
-
-def locate_princess(grid)
-  grid.each_with_index do |element, index|
-    if element.include?("p")
-      element.chars.each_with_index do |e, i|
-        if e == "p"
+        if e == character
           return [index, i]
         end
       end
@@ -44,7 +32,7 @@ end
 
 def nextMove(n,r,c,grid)
   bot_location = [r, c]
-  princess_location = locate_princess(grid)
+  princess_location = locate_character(grid, "p")
   puts make_path(n, bot_location, princess_location).split("\n").first
 end
 
